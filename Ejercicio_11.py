e1d0a99dae9413fc14bee3868a04bd66658feb20
NatyLaFlacaaa <nataliarodiciooo@gmail.com>
2024-07-06 16:19:05 -0300

    Add files via upload

---
+++ Ejercicio_11.py
@@ -0,0 +1,7 @@
+variable_A = 5
+variable_B = 10
+
+print(variable_A + variable_B)
+print(variable_A - variable_B)
+print(variable_A * variable_B)
+print(variable_A / variable_B)
---
+++ Ejercicio_7.py
@@ -0,0 +1,1 @@
+print(10/0) #Muestra error: ZeroDivisionError: division by zero
